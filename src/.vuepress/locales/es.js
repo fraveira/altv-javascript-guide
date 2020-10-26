b5dc1355fd5901fd0fb8ba953801390bce064966
fraveira <frj.riveira@gmail.com>
2020-10-26 17:00:32 +0100

    Add Spanish locales for titles

---
+++ src/.vuepress/locales/es.js
@@ -0,0 +1,84 @@
+const { defaultNavbar } = require('../defaults/navbar');
+const { buildSidebar } = require('../utility/sidebarHelper');
+const language = 'es'; // ie. /tr/
+const languageUpper = 'ES';
+const languageName = 'Spanish'; // Turkish
+
+const enLocale = {
+    [`/${language}/`]: {
+        lang: `${language}-${languageUpper}`,
+        title: `Documentation | ${languageName}`
+    }
+};
+
+const sidebar = [
+    {
+        title: 'Guía',
+        collapsable: false,
+        children: [
+            {
+                title: '🚀 Introducción',
+                collapsable: false,
+                children: buildSidebar(`/${language}/introduction/`)
+            },
+            {
+                title: `♻️ Conversión`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/conversion/`)
+            },
+            {
+                title: `📄 Guía API`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/api/`)
+            },
+            {
+                title: `💡 Eventos`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/events/`)
+            },
+            {
+                title: `🧍 Jugador`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/player/`)
+            },
+            {
+                title: `🚙 Vehículo`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/vehicle/`)
+            },
+            {
+                title: `📊 Bases de datos`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/databases/`)
+            },
+            {
+                title: `📚 Libro de Recetas `,
+                collapsable: false,
+                children: buildSidebar(`/${language}/cookbook/`)
+            },
+            {
+                title: `📖 Tablas de Datos`,
+                collapsable: false,
+                children: buildSidebar(`/${language}/tables/`)
+            }
+        ]
+    }
+];
+
+// change this to first two letters + menus. ie. trMenus
+const enMenus = {
+    [`/${language}/`]: {
+        label: languageName,
+        nav: [...defaultNavbar],
+        sidebar: {
+            collapsable: false,
+            [`/${language}/`]: sidebar
+        },
+        sidebarDepth: 3
+    }
+};
+
+module.exports = {
+    enLocale,
+    enMenus
+};
